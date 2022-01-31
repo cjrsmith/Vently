112e56b2fac4f704477585e4c57763cc9014e214
Connor Smith <cs878@kent.ac.uk>
2022-01-31 21:21:26 +0000

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Movies from "./components/movies";
+import { Route, Routes, Redirect } from "react-router-dom";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Routes>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/customers" element={<Customers />}></Route>
+        <Route path="/rentals" element={<Rentals />}></Route>
+        <Route path="/not-found" element={<NotFound />}></Route>
+      </Routes>
     </main>
   );
 }
