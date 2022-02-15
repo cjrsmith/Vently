aa881853ce15f21e1c9ea93a3105849035e1a01d
Connor Smith <cs878@kent.ac.uk>
2022-02-15 17:48:27 +0000

    Installed Joi for validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -9,7 +10,16 @@ class LoginForm extends Component {
   style = {
     marginTop: "10px",
   };
+
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
     const errors = {};
     const { account } = this.state;
     if (account.username.trim() === "")
@@ -29,10 +39,23 @@ class LoginForm extends Component {
     //Call server
     console.log("Submitted");
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
